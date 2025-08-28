abd111cc66fa69ebe0a7c39be0da2719b784f180
Jamison Lahman <jamison@lahman.dev>
2025-08-28 02:57:12 -0700

    fix python-dict2xml build

--- pkgreleaser.py
+++ pkgreleaser.py
@@ -1,35 +1,30 @@
 #!/usr/bin/env python3
+from __future__ import annotations
+
 import argparse
 import json
-import re
-import subprocess
 import os
 from pathlib import Path
+import re
+import subprocess
 from typing import NamedTuple
+
 
 class Package(NamedTuple):
     name: str
     version: str
     revision: str
 
+
 def run_nvchecker(entry: str) -> list[str]:
     result = subprocess.run(
-        [
-            "uv",
-            "run",
-            "--with=git+https://github.com/lilydjwg/nvchecker@2722ccc7fef71fccf9f031d8299bc3c36736fdda",
-            "nvchecker",
-            "--entry",
-            entry,
-            "--logger=json",
-            "-c",
-            "nvchecker.toml"
-        ],
+        ["uvx", "nvchecker", "--entry", entry, "--logger=json", "-c", "nvchecker.toml"],
         check=True,
         text=True,
         capture_output=True,
     )
     return result.stdout.splitlines()
+
 
 def parse_nvchecker_output(lines: list[str]) -> list[Package]:
     nv_data = []
@@ -39,6 +34,7 @@ def parse_nvchecker_output(lines: list[str]) -> list[Package]:
             Package(name=data["name"], version=data["version"], revision=data["revision"])
         )
     return nv_data
+
 
 def process_package(package: Package) -> None:
     dir_path = Path(package.name)
@@ -59,8 +55,10 @@ def process_package(package: Package) -> None:
         return
 
     updated_content = re.sub(r"(?m)^pkgver=(.+)$", f"pkgver={package.version}", content)
-    updated_content = re.sub(r"(?m)^pkgrel=(.+)$", f"pkgrel=1", updated_content)
-    updated_content = re.sub(r"(?m)^_commit=(.+)$", f"_commit='{package.revision}'", updated_content)
+    updated_content = re.sub(r"(?m)^pkgrel=(.+)$", "pkgrel=1", updated_content)
+    updated_content = re.sub(
+        r"(?m)^_commit=(.+)$", f"_commit='{package.revision}'", updated_content
+    )
     pkgbuild_path.write_text(updated_content)
     with srcinfo_path.open(mode="w") as f:
         subprocess.run(["makepkg", "--printsrcinfo"], stdout=f, check=True, cwd=dir_path)
@@ -69,6 +67,7 @@ def process_package(package: Package) -> None:
         subprocess.run(["updpkgsums"], check=True, capture_output=True, cwd=dir_path)
 
     print(f"Bump {package.name} from {current_version} to {package.version}")
+
 
 def _directory(value):
     if not os.path.isdir(value):
@@ -88,6 +87,7 @@ def main():
 
     if package:
         process_package(package)
+
 
 if __name__ == "__main__":
     main()

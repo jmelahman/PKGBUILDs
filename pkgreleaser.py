e9a50818e07a83da54ddba5c08c95a5d00c88398
Jamison Lahman <jamison@lahman.dev>
2026-03-17 09:43:38 -0700

    feat: use GITHUB_TOKEN in hourly

--- pkgreleaser.py
+++ pkgreleaser.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 import re
 import subprocess
+import tempfile
 from typing import NamedTuple
 
 # AUR package name to upstream package name.
@@ -29,21 +30,27 @@ class Package(NamedTuple):
 
 
 def run_nvchecker(entry: str) -> list[str]:
-    result = subprocess.run(  # noqa: S603
-        [
-            "python",
-            "-m",
-            "nvchecker",
-            "--entry",
-            ENTRY_TO_UPSTREAM.get(entry, entry),
-            "--logger=json",
-            "-c",
-            "nvchecker.toml",
-        ],
-        check=True,
-        text=True,
-        stdout=subprocess.PIPE,
-    )
+    cmd = [
+        "python",
+        "-m",
+        "nvchecker",
+        "--entry",
+        ENTRY_TO_UPSTREAM.get(entry, entry),
+        "--logger=json",
+        "-c",
+        "nvchecker.toml",
+    ]
+
+    token = os.environ.get("GITHUB_TOKEN")
+    if not token:
+        result = subprocess.run(cmd, check=True, text=True, stdout=subprocess.PIPE)  # noqa: S603
+        return result.stdout.splitlines()
+
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".toml", delete_on_close=False) as f:
+        f.write(f'[keys]\n[keys.github]\ntoken = "{token}"\n')
+        f.close()
+        cmd.extend(["--keyfile", f.name])
+        result = subprocess.run(cmd, check=True, text=True, stdout=subprocess.PIPE)  # noqa: S603
     return result.stdout.splitlines()
 
 
